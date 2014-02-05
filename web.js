0ebc0c931c12aa836c07ffbe2d844402746fcaaf
Chris Lavery <laverycs@gmail.com>
2014-02-05 00:50:45 -0500

    Made web.js read html file to text

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(
+      fs.readFileSync(index.html,encoding);
+);
 });
 
 var port = process.env.PORT || 5000;
